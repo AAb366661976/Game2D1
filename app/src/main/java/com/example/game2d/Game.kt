1b4aa76e91749ab2460f27da216760cc58c6a2c1
黃歆芝的實力 <bella12345694@gmail.com>
2024-11-18 16:47:28 +0800

    19

--- app/src/main/java/com/example/game2d/Game.kt
+++ app/src/main/java/com/example/game2d/Game.kt
@@ -9,11 +9,13 @@ class Game (val scope:CoroutineScope,val screenW:Int, val screenH: Int){
     var counter = 0
     val state = MutableStateFlow(0)
     val background = Background(screenW)
+    var isPlaying = true
 
     fun Play(){
         scope.launch {
             counter = 0
-            while (counter<2000) {
+            isPlaying = true
+            while (isPlaying) {
                 delay(4)
                 background.Roll()
                 counter++
