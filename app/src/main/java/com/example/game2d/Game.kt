6f23e2a7e5edd76f21f3905c13fae10d53509d9e
黃歆芝的實力 <bella12345694@gmail.com>
2024-11-18 16:38:31 +0800

    17

--- app/src/main/java/com/example/game2d/Game.kt
+++ app/src/main/java/com/example/game2d/Game.kt
@@ -5,15 +5,17 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.MutableStateFlow
 
-class Game (val scope:CoroutineScope){
+class Game (val scope:CoroutineScope,val screenW:Int, val screenH: Int){
     var counter = 0
     val state = MutableStateFlow(0)
+    val background = Background(screenW)
 
     fun Play(){
         scope.launch {
             counter = 0
             while (counter<2000) {
                 delay(4)
+                background.Roll()
                 counter++
                 state.emit(counter)
 
