a75ccc64e43b045d39719a1a1784e132bddd4ee6
黃歆芝的實力 <bella12345694@gmail.com>
2024-11-25 15:50:07 +0800

    34

--- app/src/main/java/com/example/game2d/Virus.kt
+++ app/src/main/java/com/example/game2d/Virus.kt
@@ -1,12 +1,18 @@
 package com.example.game2d
-
+import android.graphics.Rect
 class Virus(val screenW:Int, val screenH:Int, scale:Float) {
     var w = (80 * scale).toInt()  //病毒寬度
     var h = (80 * scale).toInt()  //病毒高度
     var x = screenW  //病毒x軸座標
     var y = (screenH/2).toInt()  //病毒y軸座標
+    var zoomout = (10 * scale).toInt()  //圖片內縮
 
     var pictNo = 0  //切換圖片
+
+    fun getRect(): Rect {
+        return Rect(x+zoomout, y+zoomout,x+w-zoomout, y+h-zoomout)
+    }
+
 
     fun Reset(){
         x = screenW
